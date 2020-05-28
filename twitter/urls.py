7584228b2e88d1623b3e7296d5daaffd627a2baf
Eric Cassidy <eric.cassidy@automatichandling.com>
2020-05-27 22:53:41 -0400

    Reset index page to twitter

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tweets/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('search/', views.search, name='search'),
 ]
