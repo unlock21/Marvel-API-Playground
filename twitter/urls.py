07a7f002ea8c331e3d141f966a77c9f4237f80cc
Eric Cassidy <eric.cassidy@automatichandling.com>
2020-05-27 23:17:02 -0400

    use django urls

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'twitter'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
