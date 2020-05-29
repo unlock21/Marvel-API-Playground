16e89ce9592594ab8d59445e069411aa376ad524
Eric Cassidy <eric.cassidy@automatichandling.com>
2020-05-28 20:53:38 -0400

    give the path a more api feel

--- twitter/urls.py
+++ twitter/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'twitter'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
+    path('api/twitter/search/', views.search, name='search'),
 ]
